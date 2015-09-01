f25e5724afdad96f4fa4004dd46f32151b02af9a
Austen <rawleag@gmail.com>
2015-09-01 11:52:17 -0700

    matrix object works

--- calculator/matrix.js
+++ calculator/matrix.js
@@ -1,34 +1,35 @@
 function matrixIt (lcd, matrixDo) {
 	var toArr = lcd.split(' ');
-
 	//determine which method to use
 	if (matrixDo === 'determ') {
-		$('#screen').text(matrixVar[determinant(toArr)]);
+		//console.log(matrixVar['determinant'])
+		//console.log(matrixVar.determinant)
+		$('#screen').text(matrixVar['determinant'](toArr));
 	} else if (matrixDo === 'dotProd') {
-		$('#screen').text(math.eval(lcd));
+		$('#screen').text(matrixVar['dotProduct'](lcd));
 	} else if (matrixDo === 'crossProd') {
-		var halfOne = toArr[0]
-		var halfTwo = toArr[1]
-		$('#screen').text('['+(math.cross(math.eval(halfOne), math.eval(halfTwo)))._data + ']');
+		$('#screen').text('['+ matrixVar['crossProd'](toArr) + ']');
 	} else {
-		$('#screen').text(math.trace(math.eval(toArr)));
+		$('#screen').text(matrixVar['trace'](toArr));
 	}
 }
 
 
 //This object doesn't currently work
-// var matrixVar = {
-// 		determinant: function (toArr) {
-// 			return math.det(math.eval(toArr));
-// 		},
-// 		dotProduct: function (lcd) {
-// 			return math.eval(lcd);
-// 		},
-// 		crossProduct: function (toArr) {
-// 			return math.crossProduct(math.eval(toArr));
-// 		},
-// 		trace: function (toArr) {
-// 			return math.trace(math.eval(toArr));
-// 		}
-// }
+var matrixVar = {
+		determinant: function (toArr) {
+			return math.det(math.eval(toArr));
+		},
+		dotProduct: function (lcd) {
+			return math.eval(lcd);
+		},
+		crossProduct: function (toArr) {
+			var halfOne = toArr[0]
+			var halfTwo = toArr[1]
+			return (math.cross(math.eval(halfOne), math.eval(halfTwo)))._data
+		},
+		trace: function (toArr) {
+			return math.trace(math.eval(toArr));
+		}
+}
 
