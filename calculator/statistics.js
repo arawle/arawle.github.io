193a68a366666c1594f71f44c1421f5c7614eae4
Austen <rawleag@gmail.com>
2015-09-01 11:57:01 -0700

    statistics object working

--- calculator/statistics.js
+++ calculator/statistics.js
@@ -1,31 +1,27 @@
 function statsIt (lcd, statsDo) {
-	var toArr = lcd.split(' ');
-
 	if (statsDo === 'mean') {
-		console.log('hi')
-		$('#screen').text(math.mean(math.eval(lcd)));
+		$('#screen').text(statsVar['mean'](lcd));
 	} else if (statsDo === 'median') {
-		$('#screen').text(math.median(math.eval(lcd)));
+		$('#screen').text(statsVar['median'](lcd));
 	} else if (statsDo === 'mode') {
-		$('#screen').text(math.mode(math.eval(lcd)));
+		$('#screen').text(statsVar['mode'](lcd));
 	} else {
-		$('#screen').text(math.std(math.eval(lcd)));
+		$('#screen').text(statsVar['std'](lcd));
 	}
 }
 
-//This object doesn't currently work
-// var statsVar = {
-// 	mean: function (toArr) {
-// 		return math.det(math.eval(toArr));
-// 	},
-// 	median: function (lcd) {
-// 		return math.eval(lcd);
-// 	},
-// 	mode: function (toArr) {
-// 		return math.crossProduct(math.eval(toArr));
-// 	},
-// 	std: function (toArr) {
-// 		return math.trace(math.eval(toArr));
-// 	}
-// }
+var statsVar = {
+	mean: function (lcd) {
+		return math.mean(math.eval(lcd));
+	},
+	median: function (lcd) {
+		return math.median(math.eval(lcd))
+	},
+	mode: function (lcd) {
+		return math.mode(math.eval(lcd))
+	},
+	std: function (toArr) {
+		return math.std(math.eval(lcd));
+	}
+}
 
