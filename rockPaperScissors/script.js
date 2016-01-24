6b75d0092db78c2eac89527bfd7ffa99e09db635
Austen <rawleag@gmail.com>
2016-01-24 07:48:14 -0800

    removed comments

--- rockPaperScissors/script.js
+++ rockPaperScissors/script.js
@@ -1,15 +1,14 @@
 $(function() {
-	//create user choice and computer choice variable, start set to zero
 	var userChoice = null;
 	var compChoice = null;
 	var lossCounter = 0;
 	var winCounter = 0;
-	//create event listeners for rock, paper, and scissors
+
 	$('.fit').on('click', function (e) {
 		computerPicker();
 		comparePicks(userChoice, compChoice);
 	});
-	// create user choice event handler function
+
 	var userPicker = $('div').on('click', function (e){
 		if ($(this).hasClass('rock')) {
 			userChoice = 'rock';
@@ -20,7 +19,7 @@ $(function() {
 		};
 		return userChoice;
 	});
-	// function for computer to choose rock, paper or scissors
+
 	function computerPicker () {
 		var computerNumber = Math.random();
 		if (computerNumber < 0.33){
@@ -32,7 +31,7 @@ $(function() {
 		};
 		return compChoice;
 	};
-	// if rock = rock, paper = paper, or scissors = scissors, = tie
+
 	function comparePicks (userChoice, compChoice) {
 		if (userChoice == compChoice) {
 			choiceDisplay('The result is a tie');
@@ -68,18 +67,17 @@ $(function() {
 			};
 		};
 	};
-	// create win/loss counter
+
 	$('#counter').html('wins - ' + winCounter + '\n' + 'losses - ' + lossCounter);
-	//function for appending win/loss counter
+
 	function winLoss () {
 		$('#counter').html('wins - ' + winCounter + '\n' + 'losses - ' + lossCounter);
-		// section.appendChild(counter);
 	};
-	//choice display function (section.choice)
+
 	function choiceDisplay (winner) {
 		$('.choiceDisplay').html('you chose ' + userChoice + ' // computer chose ' + compChoice + ' // ' + winner);
 	};
-	// create reset button for wins/losses and choice display
+
 	var button = $('button');
 	button.on('click', function(){
 	  $('#counter').html('wins - ' + 0 + ' ' + 'losses - ' + 0);
