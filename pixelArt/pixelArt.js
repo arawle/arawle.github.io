6b214fd83697f0554f47f32341c876b24a6b26ec
Austen <rawleag@gmail.com>
2016-01-24 08:42:53 -0800

    formatting

--- pixelArt/pixelArt.js
+++ pixelArt/pixelArt.js
@@ -1,6 +1,8 @@
-var currentColor = 'blue';
-
+var button = document.createElement('button');
+var currentColor = 'black';
+var checkerButton = document.createElement('button');
 var container = document.querySelector('div');
+
 container.style.width = '1000px';
 container.style.height = '760px';
 
@@ -8,6 +10,7 @@ document.querySelector('div').appendChild(document.createElement('br'));
 
 function canvas (color){
 	var newDiv = document.createElement('div');
+	var list = document.querySelectorAll('div')[0];
 	newDiv.style.backgroundColor = color;
 	newDiv.style.width = '19px';
 	newDiv.style.height = '19px';
@@ -15,7 +18,6 @@ function canvas (color){
 	newDiv.style.borderStyle = 'solid';
 	newDiv.style.borderColor = '#c8c8c8';
 	newDiv.style.borderWidth = "1px";
-
 	newDiv.addEventListener('click', function() {
 		newDiv.style.backgroundColor = currentColor;
 		newDiv.style.borderColor = currentColor;
@@ -24,39 +26,34 @@ function canvas (color){
 		newDiv.style.backgroundColor = 'white';
 		newDiv.style.borderColor = '#c8c8c8';
 	});
-	var list = document.querySelectorAll('div')[0];
 	list.appendChild(newDiv);
 }
-
 for(var i = 0; i < 1598; i++){
 	canvas('white');
 }
 
-var button = document.createElement('button');
 button.innerHTML = 'click to reset';
 button.addEventListener('click', function(){
 	location.reload('div');
 });
 document.querySelector('body').appendChild(button);
 
-var checkerButton = document.createElement('button');
+
 checkerButton.innerHTML = 'Click for a pattern!';
 checkerButton.style.float = 'left';
 checkerButton.addEventListener('click', function(){
-	var test = document.querySelectorAll('div:nth-of-type(even):not(.colorsToChooseFrom)');
-	for (var i = 0; i < test.length; i++) {
-	  test[i].style.backgroundColor === 'white' ? color = '#c8c8c8' : color = 'white';
-	  test[i].style.backgroundColor = color;
+	var checker = document.querySelectorAll('div:nth-of-type(even):not(.colorsToChooseFrom)');
+	for (var i = 0; i < checker.length; i++) {
+	  checker[i].style.backgroundColor === 'white' ? color = '#c8c8c8' : color = 'white';
+	  checker[i].style.backgroundColor = color;
 	}
 });
-
 var list = document.querySelector('body');
 list.appendChild(checkerButton);
 
 function setCurrentColor (newColor) {
 	currentColor = newColor;
 }
-
 var test = document.querySelector('.color');
 test.addEventListener('click', function(){
 	var x = this;
@@ -65,26 +62,3 @@ test.addEventListener('click', function(){
 		console.log(currentColor);
 	});
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
