70864f7d039e1b5f133b997f3308ae46f9858ba0
Austen <rawleag@gmail.com>
2016-01-24 08:22:27 -0800

    formatting

--- calculator/statistics.js
+++ calculator/statistics.js
@@ -1,26 +1,25 @@
 function statsIt (lcd, statsDo) {
-	if (statsDo === 'mean') {
-		$('#screen').text(statsVar['mean'](lcd));
-	} else if (statsDo === 'median') {
-		$('#screen').text(statsVar['median'](lcd));
-	} else if (statsDo === 'mode') {
-		$('#screen').text(statsVar['mode'](lcd));
-	} else {
-		$('#screen').text(statsVar['std'](lcd));
-	}
+  if (statsDo === 'mean') {
+    $('#screen').text(statsVar['mean'](lcd));
+  } else if (statsDo === 'median') {
+    $('#screen').text(statsVar['median'](lcd));
+  } else if (statsDo === 'mode') {
+    $('#screen').text(statsVar['mode'](lcd));
+  } else {
+    $('#screen').text(statsVar['std'](lcd));
+  }
 }
 var statsVar = {
-	mean: function (lcd) {
-		return math.mean(math.eval(lcd));
-	},
-	median: function (lcd) {
-		return math.median(math.eval(lcd))
-	},
-	mode: function (lcd) {
-		return math.mode(math.eval(lcd))
-	},
-	std: function (toArr) {
-		return math.std(math.eval(lcd));
-	}
-}
-
+  mean: function (lcd) {
+    return math.mean(math.eval(lcd));
+  },
+  median: function (lcd) {
+    return math.median(math.eval(lcd));
+  },
+  mode: function (lcd) {
+    return math.mode(math.eval(lcd));
+  },
+  std: function (toArr) {
+    return math.std(math.eval(lcd));
+  }
+};
